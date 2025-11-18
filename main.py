3806670b2cc9bc6d28f752469e90708d438a62c3
Erik Peteri <peteri.erik@gmail.com>
2025-11-18 19:52:24 +0200

    Create rastrigin function and plot the one dimensional version

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
 
+def rastrigin(X: np.ndarray, A=10):
+    n = X.shape[-1] 
+    return A*n + np.sum(X**2 - A*np.cos(2*np.pi*X), axis=-1)  
+
+x = np.linspace(-5.12, 5.12, 400)
+X = x[:, np.newaxis]
+y = rastrigin(X)
+
+plt.plot(X, y)
+plt.show()
